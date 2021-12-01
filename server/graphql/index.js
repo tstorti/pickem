89c9be25feb2588d8cedbdb08ec68d33174458ab
Tony Storti <tonystorti@gmail.com>
2021-12-01 12:21:50 -0600

    switch to es6 modules, add graphql

---
+++ server/graphql/index.js
@@ -0,0 +1,15 @@
+import { buildSchema } from 'graphql';
+
+//construct basic schema
+export const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+//resolver function
+export const root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
--- server/index.js
+++ server/index.js
@@ -1,5 +1,12 @@
-const path = require('path');
-const express = require("express");
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { schema, root } from './graphql/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const PORT = process.env.PORT || 3001;
 
@@ -8,8 +15,15 @@ const app = express();
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-app.get("/health", (req, res) => {
-  res.json({ message: "Hello from server!" });
+//add graphql endpoint
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.get('/health', (req, res) => {
+  res.json({ message: 'Hello from server!' });
 });
 
 app.listen(PORT, () => {
